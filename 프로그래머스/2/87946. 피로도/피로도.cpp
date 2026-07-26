0e08aeb53dd3be3397085af86f4adb453b7dfc10
sohnseo24 <sohnseo24@gmail.com>
2026-07-26 16:38:44 +0900

    [level 2] Title: 피로도, Time: 0.42 ms, Memory: 4.79 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,32 +1,22 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-
 using namespace std;
-
-int solution(int k, vector<vector<int>> dungeons) {
-    int n=dungeons.size();
-    vector<int> v;//순열을 돌려야 하니까(문자열/벡터)에만 가능 
+int answer=0;//전역변수로 최댓값 저장 
+void dfs(int n, int k, vector<int>& v, const vector<vector<int>>& dungeons,int count){
+    answer=max(answer,count);//매 재귀마다 현재까지 탐색한 던전수로 최댓값 갱신 
     for(int i=0; i<n; i++){
-        v.push_back(i);
+        if(v[i]==1 && k>=dungeons[i][0]){//아직 방문하지 않았고 갈 수 있는 던전이라면  
+            v[i]=0;//방문처리
+            dfs(n, k-dungeons[i][1], v, dungeons, count+1);//재귀호출
+            v[i]=1;//원상복구(백트레킹)
+        }
+        
     }
-    sort(v.begin(), v.end());
-    int max=0;
-    do{
-        int current_k=k;
-        int sum=0;
-        for(int i=0; i<n; i++){
-            if(current_k>=dungeons[v[i]][0]){
-                current_k-=dungeons[v[i]][1];
-                sum++;
-            }
-            else{
-                break;
-                }
-        }
-        if(max<=sum){
-            max=sum;
-        }
-    }while(next_permutation(v.begin(), v.end()));
-    return max;
+}
+int solution(int k, vector<vector<int>> dungeons) {
+    answer=0;//전역변수 초기화를 해주어야 함!
+    int n=dungeons.size();
+    vector<int> visited(n,1);
+    dfs(n, k, visited, dungeons, 0);//0으로 시작 
+    return answer;//최종 최댓값 반환 
 }
