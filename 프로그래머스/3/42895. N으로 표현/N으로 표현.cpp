1304f4bcb9367f66a122fc220b442e87ba1a2aa8
sohnseo24 <sohnseo24@gmail.com>
2026-08-14 16:56:26 +0900

    [level 3] Title: N으로 표현, Time: 2.23 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
@@ -4,35 +4,32 @@
 using namespace std;
 
 int solution(int N, int number) {
-    if (N == number) return 1;
-
-    vector<unordered_set<int>> DP(9);
-
-    int temp = 0;
-    for (int i = 1; i <= 8; i++) {
-        temp = temp * 10 + N;
-        DP[i].insert(temp);
-    }
-
-    for (int i = 1; i <= 8; i++) {
-        for (int j = 1; j < i; j++) {
-            for (int a : DP[j]) {
-                for (int b : DP[i - j]) {
-                    DP[i].insert(a + b);
-                    DP[i].insert(a - b);
-                    DP[i].insert(a * b);
-                    
-                    if (b != 0) {
-                        DP[i].insert(a / b);
+    unordered_set<int> DP[9];//주머니 만들기
+    for(int i=1; i<9; i++){
+        //이어붙인 숫자 넣기 
+        int num=0;
+        for(int j=0; j<i; j++){
+            num=num*10+N;
+        }
+        DP[i].insert(num);
+        //사칙 연산 하기
+        for(int j=1; j<i; j++){
+                for(int a: DP[j]){
+                    for(int b: DP[i-j]){
+                        if(b!=0){
+                            DP[i].insert(a/b);
+                        }
+                        DP[i].insert(a*b);
+                        DP[i].insert(a+b);
+                        DP[i].insert(a-b);
                     }
                 }
             }
-        }
         
-        if (DP[i].count(number)) {
+        //검사하기 
+        if(DP[i].count(number)){
             return i;
         }
     }
-
     return -1;
 }
